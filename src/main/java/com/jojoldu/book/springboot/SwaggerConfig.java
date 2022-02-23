24cf91e336317648b702dd0026035b2740b8cb12
Talisker25 <cth8787@gmail.com>
2022-02-23 23:41:06 +0900

    - 118 페이지까지 완료  - swagger 적용 테스트 중

---
+++ src/main/java/com/jojoldu/book/springboot/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    String version = "V2";
+    String title = "TEST API " + version;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .groupName(version)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.swagger.sample.api"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo(title, version));
+    }
+
+    private ApiInfo apiInfo(String title, String version) {
+        return new ApiInfoBuilder()
+                .title(title)
+                .version(version)
+                .description("sample swagger 입니다.")
+                .build();
+    }
+}
---
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("cth8787@gmail.com")
+                .build());
+    }
+}
